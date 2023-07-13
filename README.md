67dcb116e98050450b4313168aaff5d4841427ba
Clóvis Chakrian <clovischakriandev@gmail.com>
2023-07-13 17:09:51 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# FalaDora
+Fala + CalculaDora tendeu tendeu? Rsrs (péssimo trocadilho eu sei eu sei)
+
+## Uma calculadora que aceita comandos de voz para fazer contas. Desenvolvida com HTML5, CSS3 e JS.
+
+### Link para acessar online
+
+### Como rodar
+
+Para rodar localmente, a única coisa necessária é: um navegador. É isso mesmo, você só vai precisar baixar os arquivos deste repositório, deixá-los em uma mesma pasta e abrir o index.html com seu navegador favorito.
+
+### Como usar
+
+Ela é uma calculadora normal como qualquer outra. Em questão de áudio, primeiro vai ser necessário habilitar o microfone, para habilitar primeiro clique em mic e depois dê permissão no pop-up que vai aparecer. Após isso é só falar a conta que você deseja efetuar. Como por exemplo: '2+2*2' e aguardar. Tan-dan, o resultado estará magicamente no campo de resultado (magicamente não, há um pouco de JS por trás).
+
+### Porque isso foi desenvolvido
+Essa resposta é simples: insônia + paixão por codar.
+
+Feito com amor por Clóvis Chakrian.
---
+++ script.js
@@ -0,0 +1,81 @@
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+window.SpeechGrammarList = window.SpeechGrammarList || window.webkitSpeechGrammarList;
+const recognition = new window.SpeechRecognition();
+recognition.lang = 'pt-BR';
+
+const buttons = ['7', '8', '9', '/', '6', '5', '4', '*', '1', '2', '3', '-', 'mic', '0', '.', '+', 'AC', '=']
+const buttonsDiv = document.getElementById('buttons');
+let equation = '';
+const previousEquationElement = document.getElementById('previous');
+let previousEquationResult = '';
+
+for (let button = 0; button < buttons.length; button++) {
+  const buttonElement = document.createElement('button');
+  buttonElement.innerText = buttons[button];
+  buttonElement.id = buttons[button] === '=' ? 'eq' : buttons[button];
+  buttonElement.style.backgroundColor = isNaN(Number(buttons[button])) ? '#FFA000' : '#E5E5E5'
+  buttonsDiv.appendChild(buttonElement);
+};
+
+const resultElement = document.getElementById('result');
+const micBtn = document.getElementById('mic');
+const buttonsElement = document.querySelectorAll('button');
+
+buttonsElement.forEach(button => {
+  button.addEventListener('click', () => {
+    if (button.innerText === 'mic') {
+      return
+    };
+
+    if (button.innerText === '=') {
+      equation !== '' ? resultElement.innerText = eval(equation) : '';
+      previousEquationResult = eval(equation);
+      previousEquationElement.innerText = equation;
+      equation = '';
+      return;
+    };
+
+    if (button.innerText === 'AC') {
+      resultElement.innerText = '0';
+      equation = '';
+      previousEquationResult = '';
+      previousEquationElement = '';
+      return
+    }
+
+    if (isNaN(button.innerText) && previousEquationResult !== '') {
+      equation = previousEquationResult + button.innerText;
+      resultElement.innerText = equation;
+      previousEquationResult = '';
+      return;
+    };
+
+    if (previousEquationResult !== '') {
+      equation = equation + button.innerText;
+      resultElement.innerText = equation;
+      previousEquationResult = '';
+      return;
+    };
+
+    equation = equation + button.innerText;
+    resultElement.innerText = equation;
+  })
+})
+
+micBtn.onclick = () => {
+  recognition.start();
+};
+
+recognition.onspeechend = (e) => {
+};
+
+recognition.addEventListener("result", (event) => {
+  const conta = event.results[0][0].transcript;
+  console.log(event);
+  const resultado = eval(conta.replaceAll('x', '*'));
+  console.log(event);
+  console.log(conta);
+  console.log(conta + ' = ' + resultado);
+  previousEquationElement.innerText = conta;
+  resultElement.innerText = resultado;
+});
